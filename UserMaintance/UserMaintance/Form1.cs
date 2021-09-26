b883761358e1e9346c7647cdbfe74b1dff2c40c8
Dau Quang Phong <quang.dau@stud.uni-corvinus.hu>
2021-09-26 18:06:06 +0200

    6.feladat

--- UserMaintance/UserMaintance/Form1.cs
+++ UserMaintance/UserMaintance/Form1.cs
@@ -12,12 +12,27 @@ namespace UserMaintance
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.User> users = new BindingList<Entities.User>();
         public Form1()
         {
             InitializeComponent();
+
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new Entities.User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
         }
     }
 }
