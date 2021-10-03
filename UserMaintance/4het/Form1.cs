9da2a41cd2740759612a3d8a41140dc0d2b8e7c7
Dau Quang Phong <quang.dau@stud.uni-corvinus.hu>
2021-10-04 00:17:12 +0200

    6 feladat

--- UserMaintance/4het/Form1.cs
+++ UserMaintance/4het/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace _4het
 {
@@ -15,7 +17,9 @@ namespace _4het
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
-
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +29,42 @@ namespace _4het
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateTable()
+        { 
+            
+        }
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errorMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errorMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+
+                xlApp.Quit();
+
+                xlWB = null;
+
+                xlApp = null;
+
+                throw;
+            }
         }
     }
 }
