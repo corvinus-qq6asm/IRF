cc25107bb246821e44a95e63461b5de5efe8cd85
Dau Quang Phong <quang.dau@stud.uni-corvinus.hu>
2021-11-02 22:22:20 +0100

    week04

--- UserMaintance/week08/Form1.cs
+++ UserMaintance/week08/Form1.cs
@@ -12,9 +12,37 @@ namespace week08
 {
     public partial class Form1 : Form
     {
+        List<Entities.Ball> _balls = new List<Entities.Ball>();
+
+        Entities.BallFactory _factory;
+
+        Entities.BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new Entities.BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            ball.Left = -ball.Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (Entities.Ball item in _balls)
+            { 
+                
+            }
         }
     }
 }
