10833148a3a01a0d62ab658fbf404df53d3f93dc
Dau Quang Phong <quang.dau@stud.uni-corvinus.hu>
2021-11-12 17:38:06 +0100

    kesz

--- UserMaintance/week08/Entities/CarFactory.cs
+++ UserMaintance/week08/Entities/CarFactory.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using week08.Abstractions;
+using System.Drawing;
 
 namespace week08.Entities
 {
     class CarFactory : Abstractions.IToyFactory
     {
+        public Color BallColor { get; set; }
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Ball(BallColor);
         }
     }
 }
---
+++ UserMaintance/week08/Present.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week08
+{
+    class Present:Abstractions.Toy
+    {
+        public Present()
+        {
+                
+        }
+
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
