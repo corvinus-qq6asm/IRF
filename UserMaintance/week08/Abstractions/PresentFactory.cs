2e7c12ac45817f23368ac62471bb70bc03c2d856
Dau Quang Phong <quang.dau@stud.uni-corvinus.hu>
2021-11-12 18:33:20 +0100

    utolso

---
+++ UserMaintance/week08/Abstractions/PresentFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace week08.Abstractions
+{
+    class PresentFactory : Abstractions.IToyFactory
+    {
+        public Color Colour1 { get; set; }
+        public Color Colour2 { get; set; }
+        public Toy CreateNew()
+        {
+            return new Entities.Present(Colour1, Colour2);
+        }
+    }
+}
---
+++ UserMaintance/week08/Entities/Present.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week08
+{
+    class Present:Abstractions.Toy
+    {
+        public SolidBrush PresentColour1 { get; set; }
+        public SolidBrush PresentColour2 { get; set; }
+        public Present(Color colour1, Color colour2)
+        {
+            PresentColour1 = new SolidBrush(colour1);
+            PresentColour2 = new SolidBrush(colour2);
+        }
+
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillRectangle(PresentColour1,0,0,Width,Height);
+            g.FillRectangle(PresentColour1, 0, (Height/5)*2, Width, Height/5);
+            g.FillRectangle(PresentColour1, 0, (Width/5)*2, Width/5, Height);
+        }
+
+
+    }
+}
